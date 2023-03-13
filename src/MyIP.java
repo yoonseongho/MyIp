ef7122321cfedbb72483a7bd37f510e936fbbf25
SeeQ <sxdz1290@naver.com>
2023-03-13 15:32:02 +0900

    MyIp

--- src/MyIP.java
+++ src/MyIP.java
@@ -1,5 +1,28 @@
-public class Main {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import javax.swing.*;
+
+public class MyIP extends JFrame {
+
+    private JLabel ipLabel;
+
+    public MyIP() {
+        ipLabel = new JLabel();
+        add(ipLabel);
+        setTitle("My IP Address");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        pack();
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            String ipAddress = ip.getHostAddress();
+            new MyIP().ipLabel.setText("My IP Address: " + ipAddress);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 }
